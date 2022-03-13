ebc94423d8eedb495b6fede401416be80dee006a
jakob <jakobbraun6@gmail.com>
2022-03-13 04:27:31 +0100

    add: basic inforamtion page functions

--- src/views/EventPage.tsx
+++ src/views/EventPage.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {
-    IonContent,
+    IonCol,
+    IonContent, IonGrid,
     IonHeader, IonItem,
     IonLabel,
     IonList,
     IonListHeader,
-    IonPage, IonThumbnail,
+    IonPage, IonRow, IonThumbnail,
     IonTitle,
     IonToolbar,
 } from "@ionic/react";
@@ -30,57 +31,75 @@ const EventPage: React.FC = () => (
                         <IonLabel>Nearby</IonLabel>
                         <IonLabel class='showAllLabel'>Show all &gt;</IonLabel>
                     </IonListHeader>
-                    {nearbyEvents.events.map((occasion: any) => (
-                        <IonItem key={occasion.name} onClick={() => console.log(occasion.name)}
-                                 button>
-                            <IonThumbnail slot="start">
-                                <img src={city} alt={'image of new york'}/>
-                            </IonThumbnail>
-                            <IonLabel>
-                                <h2>{occasion.name}</h2>
-                                <p>{occasion.location.id}</p>
-                                <p>23</p>
-                            </IonLabel>
-                        </IonItem>
-                    ))}
+                    <IonGrid>
+                        <IonRow>
+                            {nearbyEvents.events.map((occasion: any) => (
+                                <IonCol key={occasion.id}>
+                                    <IonItem onClick={() => console.log(occasion.name)}
+                                             button>
+                                        <IonThumbnail slot="start">
+                                            <img src={city} alt={'image of new york'}/>
+                                        </IonThumbnail>
+                                        <IonLabel>
+                                            <h2>{occasion.name}</h2>
+                                            <p>{occasion.location.id}</p>
+                                            <p>23</p>
+                                        </IonLabel>
+                                    </IonItem>
+                                </IonCol>
+                            ))}
+                        </IonRow>
+                    </IonGrid>
                 </IonList>
                 <IonList>
                     <IonListHeader>
                         <IonLabel>Hosted by Mentors</IonLabel>
                         <IonLabel class='showAllLabel'>Show all &gt;</IonLabel>
                     </IonListHeader>
-                    {hostedByMentors.events.map((occasion: any) => (
-                        <IonItem key={occasion.name} onClick={() => console.log(occasion.name)}
-                                 button>
-                            <IonThumbnail slot="start">
-                                <img src={city} alt={'image of new york'}/>
-                            </IonThumbnail>
-                            <IonLabel>
-                                <h2>{occasion.name}</h2>
-                                <p>{occasion.location.id}</p>
-                                <p>23</p>
-                            </IonLabel>
-                        </IonItem>
-                    ))}
+                    <IonGrid>
+                        <IonRow>
+                            {hostedByMentors.events.map((occasion: any) => (
+                                <IonCol key={occasion.id}>
+                                    <IonItem onClick={() => console.log(occasion.name)}
+                                             button>
+                                        <IonThumbnail slot="start">
+                                            <img src={city} alt={'image of new york'}/>
+                                        </IonThumbnail>
+                                        <IonLabel>
+                                            <h2>{occasion.name}</h2>
+                                            <p>{occasion.location.id}</p>
+                                            <p>23</p>
+                                        </IonLabel>
+                                    </IonItem>
+                                </IonCol>
+                            ))}
+                        </IonRow>
+                    </IonGrid>
                 </IonList>
                 <IonList>
                     <IonListHeader>
                         <IonLabel>In your City</IonLabel>
                         <IonLabel class='showAllLabel'>Show all &gt;</IonLabel>
                     </IonListHeader>
-                    {inYourCity.events.map((occasion: any) => (
-                        <IonItem key={occasion.name} onClick={() => console.log(occasion.name)}
-                                 button>
-                            <IonThumbnail slot="start">
-                                <img src={city} alt={'image of new york'}/>
-                            </IonThumbnail>
-                            <IonLabel>
-                                <h2>{occasion.name}</h2>
-                                <p>{occasion.location.id}</p>
-                                <p>23</p>
-                            </IonLabel>
-                        </IonItem>
-                    ))}
+                    <IonGrid>
+                        <IonRow>
+                            {inYourCity.events.map((occasion: any) => (
+                                <IonCol key={occasion.id}>
+                                    <IonItem onClick={() => console.log(occasion.name)}
+                                             button>
+                                        <IonThumbnail slot="start">
+                                            <img src={city} alt={'image of new york'}/>
+                                        </IonThumbnail>
+                                        <IonLabel>
+                                            <h2>{occasion.name}</h2>
+                                            <p>{occasion.location.id}</p>
+                                            <p>23</p>
+                                        </IonLabel>
+                                    </IonItem>
+                                </IonCol>
+                            ))}
+                        </IonRow>
+                    </IonGrid>
                 </IonList>
             </IonContent>
         </IonPage>
